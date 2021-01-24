474258259e2bda607074d13d2532b740b92c7461
jiwon <jidole041214@naver.com>
2021-01-24 22:21:23 +0900

    CREATE : Factory add UI complete

---
+++ src/COMPONENTS/FACTORY/FACTORYADD/factoryAdd.js
@@ -0,0 +1,44 @@
+import {Request} from '../../../HOOKS/axios'
+import * as s from './styles'
+import {useState} from 'react'
+import axios from 'axios'
+import {Link} from 'react-router-dom'
+
+const FactoryAdd =()=>{
+    const [data, setData] = useState({
+        name: "",
+        location: "",
+        repName:"",
+        repPhone:"",
+        repLv:""
+    })
+    const InputInfor =(e)=>{
+        const {name, value} = e.target
+        setData({
+            ...data,
+            [name] : value
+        })
+        console.log(data)
+    }
+
+    const SubData=()=>{
+        Request("POST","clients",{"name" : data.name,"location" : data.location,"repr_name" : data.repName,"repr_phone" : data.repPhone,"repr_title" : "대표 직함"},{"Authorization": "Bearer " + window.localStorage.getItem("token"),"Content-Type": "application/json"},"공장등록")  
+    }
+    return(
+        <>
+        <s.Line></s.Line>
+        <s.Container>
+            <s.Title><b>E</b>ASY <b>A</b>IR 공장 등록</s.Title>
+            <s.SubTitle>팀에 공장을 추가합니다.</s.SubTitle>
+            <s.Input placeholder="공장명을 입력하세요." name="name" onChange={InputInfor}></s.Input>
+            <s.Input placeholder="공장 주소를 입력하세요."name="location"onChange={InputInfor}></s.Input>
+            <s.Input placeholder="대표명을 입력하세요."name="repName"onChange={InputInfor}></s.Input>
+            <s.Input placeholder="대표 전화번호를 입력하세요."name="repPhone"onChange={InputInfor}></s.Input>
+            <s.Input placeholder="대표 직함을 입력하세요."name="repLv"onChange={InputInfor}></s.Input>
+            <Link to="factory"><s.SubButton onClick={SubData}>등록하기</s.SubButton></Link>
+        </s.Container>
+        </>
+    )
+}
+
+export default FactoryAdd;
---
+++ src/COMPONENTS/FACTORY/FACTORYADD/styles.js
@@ -0,0 +1,56 @@
+import styled from "styled-components"
+import {color, blue} from '../../../PUBLIC/index'
+
+export const Line = styled.div`
+width:100%;
+border-top:1px solid rgb(200,200,200);
+margin-top:4%;
+min-width:1900px;
+position:absolute;
+`
+
+export const Container = styled.div`
+width:100%;
+height:86%;
+display:flex;
+justify-content:center;
+align-items:center;
+flex-direction:column;
+`
+
+export const Title = styled.a`
+font-size:33px;
+color:black;
+font-weight:bold;
+b{
+    color:${color};
+}
+`
+
+export const SubTitle = styled.a`
+font-size:17px;
+color:gray;
+margin-top:20px;
+margin-bottom:20px;
+`
+
+export const Input = styled.input`
+width:360px;
+height:50px;
+padding:0 20px;
+border-radius:3px;
+background-color:rgb(100,100,100,0.02);
+border:1px solid rgb(100,100,100,0.3);
+margin-top:13px;
+`
+
+export const SubButton = styled.button`
+width:400px;
+height:50px;
+margin-top:30px;
+border:none;
+background-color:${blue};
+color:white;
+font-size:18px;
+font-weight:bold;
+`
