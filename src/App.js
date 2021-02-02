88b8ee3940aa1584f480ce772a0f4d176c3ad201
jiwon <jidole041214@naver.com>
2021-02-02 21:38:52 +0900

    FIX : Router Match

--- src/App.js
+++ src/App.js
@@ -4,19 +4,26 @@ import Login from './COMPONENTS/LOGIN/login'
 import Header from './COMPONENTS/HEADER/header'
 import Overview from './COMPONENTS/OVERVIEW/overview'
 import {BrowserRouter,Route} from 'react-router-dom'
+import FactoryMange from './COMPONENTS/FACTORY/FACTORYMANAGE/factoryManage';
+import FactoryAdd from './COMPONENTS/FACTORY/FACTORYADD/factoryAdd'
+import MemberManage from './COMPONENTS/MEMBER/MEMBERMANAGE/memberManage'
+import Team from './COMPONENTS/TEAM/team'
 
 function App() {
   return (
     <BrowserRouter>
     <GlobalStyle/>
     <Route exact path="/"><Login></Login></Route>
-    <Route exact path="/overview">
-        <Header></Header>
-        <Overview></Overview>
-    </Route>
-{/*         <Login></Login> */}
+    <Route path="/" component={Header}></Route>
+    <Route path="/overview" component={Overview}></Route>
+    <Route path="/factory" component={FactoryMange}></Route>
+    <Route path="/addfactory"><FactoryAdd></FactoryAdd></Route>
+    <Route path="/member"><MemberManage></MemberManage></Route>
+    <Route path="/team"><Team></Team></Route>
+    <Route path="/check"></Route>
     </BrowserRouter>
   );
 }
 
 export default App;
+  
