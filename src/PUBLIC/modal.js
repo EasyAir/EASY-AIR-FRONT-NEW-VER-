d5a21e699a0b4273bbed6a41f43ea483c62946a8
jiwon <jidole041214@naver.com>
2021-02-13 17:01:51 +0900

    CREATE : context api 모달 연결

---
+++ src/PUBLIC/modal.js
@@ -0,0 +1,19 @@
+import MemberModal from '../COMPONENTS/CHECK/memberModal'
+import * as s from './styles'
+import {useContext} from 'react'
+import {Context} from '../CONTEXT/provider'
+
+const Modal =()=>{
+    const {modalName, contextDispatch} = useContext(Context);
+    return(
+        <>
+        {(modalName == "none")?<></>
+        :(modalName == "writer")?<s.Cover 
+            onClick={()=>contextDispatch({type:"change",value:"none"})}
+        ><MemberModal></MemberModal></s.Cover>
+        :<></>}
+        </>
+    )
+}
+
+export default Modal;
