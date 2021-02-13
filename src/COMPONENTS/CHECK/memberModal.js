682155a092f386ce3f7ac725c43a42691e7bcf80
jiwon <jidole041214@naver.com>
2021-02-13 17:01:24 +0900

    CREATE : 직원 정보 모달

---
+++ src/COMPONENTS/CHECK/memberModal.js
@@ -0,0 +1,20 @@
+import * as s from './styles'
+import 프로필 from "../../IMG/프로필.png"
+
+export default function MemberModal(){
+    return(
+        <s.Cover>
+            <s.MModalContainer>
+                <s.MHeader>직원 프로필</s.MHeader>
+                <s.MMain>
+                    <img src={프로필} alt="" />
+                    <s.MProfileIntro>
+                        <p>정지원<b>TEAM 지원공장</b></p>
+                        <a>email : jidole041241@navr.com</a>
+                        <a>phone : 010 2813 2732</a>
+                    </s.MProfileIntro>
+                </s.MMain>
+            </s.MModalContainer>
+        </s.Cover>
+    )
+}
