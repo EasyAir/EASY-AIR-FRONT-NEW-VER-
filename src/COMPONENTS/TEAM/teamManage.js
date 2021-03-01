cfe960f9f34d902b6b186c880a0082cae2203d2d
jiwon <jidole041214@naver.com>
2021-03-01 22:13:26 +0900

    UPDATE : 팀 정보 서버 연동

--- src/COMPONENTS/TEAM/teamManage.js
+++ src/COMPONENTS/TEAM/teamManage.js
@@ -1,34 +1,52 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Request } from '../../HOOKS/axios';
+import Loading from '../../PUBLIC/loading';
 import * as s from './styles'
 
 const TeamManage=()=>{
+    const [team,setTeam] = useState({
+        name:"",
+        code:"",
+        client_count:"",
+        compressor_count:"",
+        server_count:""
+    });
+
+    const [loading,setLoading] = useState(true);
+
     useEffect(()=>{
         Request("get","team",{},{"Authorization": "Bearer "+ window.localStorage.getItem("token"), "Content-Type": "application/json"},"팀 상세정보")
-    })
+        .then((e)=>{
+            setTeam(e)
+            setLoading(false)
+        })
+    },[])
+
     return(
         <>
+        { loading && <Loading/> }
         <s.Line/>
         <s.Container style={{height:"80%"}}> 
-            <s.TeamName>TEAM 병진째찌</s.TeamName>
-            <s.TeamCode>CODE : 12312314124ADFA</s.TeamCode>
+            <s.TeamName>TEAM {team.name}</s.TeamName>
+            <s.TeamCode>CODE : {team.code}</s.TeamCode>
             <s.DetailMenu>
                 <s.DCnt>
                     <s.DetailTitle>공장 수</s.DetailTitle>
-                    <s.Cnt>60 개</s.Cnt>
+                    <s.Cnt>{team.client_count} 개</s.Cnt>
                 </s.DCnt>
                 <s.DCnt>
                     <s.DetailTitle>공기압축기 수</s.DetailTitle>
-                    <s.Cnt>60 개</s.Cnt>
+                    <s.Cnt>{team.compressor_count} 개</s.Cnt>
                 </s.DCnt>
                 <s.DCnt>
                     <s.DetailTitle>직원 수</s.DetailTitle>
-                    <s.Cnt>60 명</s.Cnt>
+                    <s.Cnt>{team.server_count} 명</s.Cnt>
                 </s.DCnt>
             </s.DetailMenu>
             <s.ReplaceBtn
                 onClick={()=>{
                     Request("post","code",{},{"Authorization": "Bearer "+window.localStorage.getItem("token")},"팀 코드 변경")
+                    window.location.href = "/teammanage"
                 }}
             >팀 코드 변경</s.ReplaceBtn>
         </s.Container>
